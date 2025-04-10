4bcd54c7025551399c2932d37e8db53800f41cc8
gbsxl <gasousaoli@gmail.com>
2025-04-10 16:00:42 -0300

    add: create WebClient on path Config to make connection with AI on the future

---
+++ src/main/java/dev/gbs/StoryGeneratorAI/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package dev.gbs.StoryGeneratorAI.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Value("${chatgpt.api.url:https://api.openai.com/v1/chat/completions}")
+    private String chatGptUrl;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder){
+        return builder.baseUrl(chatGptUrl).build();
+    }
+}
